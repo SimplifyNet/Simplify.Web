9a5c52bfa612b5f1309be7362ff96442f2521373
Alexanderius <alexander.krylkov@gmail.com>
2019-10-13 21:57:48 +0600

    Initial validation attribute base class
    Initial refactored RequiredAttribute

--- src/Simplify.Web/Model/Validation/Attributes/RequiredAttribute.cs
+++ src/Simplify.Web/Model/Validation/Attributes/RequiredAttribute.cs
@@ -5,8 +5,30 @@ namespace Simplify.Web.Model.Validation.Attributes
 	/// <summary>
 	/// Indicates what this property should be not null or empty
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Property)]
-	public class RequiredAttribute : Attribute
+	//[AttributeUsage(AttributeTargets.Property)]
+	public class RequiredAttribute : ValidationAttribute
 	{
+		private readonly string _stringTableKey;
+		private readonly bool _messageFromStringTable;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RequiredAttribute"/> class.
+		/// </summary>
+		/// <param name="stringTableKey">The string table key.</param>
+		/// <param name="messageFromStringTable">if set to <c>true</c> [message from string table].</param>
+		public RequiredAttribute(string stringTableKey = null, bool messageFromStringTable = true)
+		{
+			_stringTableKey = stringTableKey;
+			_messageFromStringTable = messageFromStringTable;
+		}
+
+		/// <summary>
+		/// Validates the specified property value.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		public override void Validate(object value)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
---
+++ src/Simplify.Web/Model/Validation/Attributes/ValidationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Simplify.Web.Model.Validation.Attributes
+{
+	/// <summary>
+	/// Represent model property validation attribute base class
+	/// </summary>
+	/// <seealso cref="Attribute" />
+	[AttributeUsage(AttributeTargets.Property)]
+	public abstract class ValidationAttribute : Attribute
+	{
+		/// <summary>
+		/// Validates the specified property value.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		public abstract void Validate(object value);
+	}
+}
