d75a19086a7725f309abf9cf40365d4f356b9d3c
Alexanderius <alexander.krylkov@gmail.com>
2019-10-15 22:43:48 +0600

    Required tests revisit

--- src/Simplify.Web.Tests/Model/Validation/ValidationAttributesExecutorTests.cs
+++ src/Simplify.Web.Tests/Model/Validation/ValidationAttributesExecutorTests.cs
@@ -13,57 +13,5 @@ namespace Simplify.Web.Tests.Model.Validation
 		{
 			_validator = new ValidationAttributesExecutor();
 		}
-
-		// TODO refactor
-
-		//[Test]
-		//public void Validate_UserTypeRequiredFieldIsNullOtherIsOk_ExceptionThrown()
-		//{
-		//	// Assign
-		//	var model = new TestModelRequired { Prop1 = "test", Prop2 = new List<string>() };
-
-		//	// Act & Assert
-		//	Assert.Throws<ModelValidationException>(() => _validator.Validate(model));
-		//}
-
-		//[Test]
-		//public void Validate_RequiredCustomArrayFieldIsNull_ExceptionThrown()
-		//{
-		//	// Assign
-		//	var model = new TestModelRequiredCustomArray();
-
-		//	// Act & Assert
-		//	Assert.Throws<ModelValidationException>(() => _validator.Validate(model));
-		//}
-
-		//[Test]
-		//public void Validate_RequiredCustomArrayFieldIsNotNull_NoExceptions()
-		//{
-		//	// Assign
-		//	var model = new TestModelRequiredCustomArray { Prop1 = new TestModelEMail[1] { null } };
-
-		//	// Act & Assert
-		//	_validator.Validate(model);
-		//}
-
-		//[Test]
-		//public void Validate_RequiredCustomListFieldIsNull_ExceptionThrown()
-		//{
-		//	// Assign
-		//	var model = new TestModelRequiredCustomGenericList();
-
-		//	// Act & Assert
-		//	Assert.Throws<ModelValidationException>(() => _validator.Validate(model));
-		//}
-
-		//[Test]
-		//public void Validate_RequiredCustomListFieldIsNotNull_NoExceptions()
-		//{
-		//	// Assign
-		//	var model = new TestModelRequiredCustomGenericList() { Prop1 = new List<TestModelEMail>() };
-
-		//	// Act & Assert
-		//	_validator.Validate(model);
-		//}
 	}
 }
