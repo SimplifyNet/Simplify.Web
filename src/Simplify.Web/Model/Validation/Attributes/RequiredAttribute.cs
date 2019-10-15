76a25c06c015089e3c4bc9022d3572a1d5082231
Alexanderius <alexander.krylkov@gmail.com>
2019-10-15 22:59:32 +0600

    Shared code moved to base class

--- src/Simplify.Web/Model/Validation/Attributes/RequiredAttribute.cs
+++ src/Simplify.Web/Model/Validation/Attributes/RequiredAttribute.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Reflection;
 using Simplify.DI;
-using Simplify.Web.Modules.Data;
 
 namespace Simplify.Web.Model.Validation.Attributes
 {
@@ -11,18 +10,13 @@ namespace Simplify.Web.Model.Validation.Attributes
 	[AttributeUsage(AttributeTargets.Property)]
 	public class RequiredAttribute : ValidationAttribute
 	{
-		private readonly string _errorMessage;
-		private readonly bool _isMessageFromStringTable;
-
 		/// <summary>
 		/// Initializes a new instance of the <see cref="RequiredAttribute" /> class.
 		/// </summary>
 		/// <param name="errorMessage">The custom error message, should contain string table item key if messageFromStringTable is true.</param>
 		/// <param name="isMessageFromStringTable">if set to <c>true</c> then indicates that errorMessage is containing string table item key instead of string error message.</param>
-		public RequiredAttribute(string errorMessage = null, bool isMessageFromStringTable = true)
+		public RequiredAttribute(string errorMessage = null, bool isMessageFromStringTable = true) : base(errorMessage, isMessageFromStringTable)
 		{
-			_errorMessage = errorMessage;
-			_isMessageFromStringTable = isMessageFromStringTable;
 		}
 
 		/// <summary>
@@ -53,13 +47,9 @@ namespace Simplify.Web.Model.Validation.Attributes
 			if (objectIsValid)
 				return;
 
-			if (string.IsNullOrEmpty(_errorMessage))
-				throw new ModelValidationException($"Required property '{propertyInfo.Name}' is null or empty");
+			TryThrowCustomOrStringTableException(resolver);
 
-			if (_isMessageFromStringTable)
-				throw new ModelValidationException(resolver.Resolve<IStringTable>().GetItem(_errorMessage));
-
-			throw new ModelValidationException(_errorMessage);
+			throw new ModelValidationException($"Required property '{propertyInfo.Name}' is null or empty");
 		}
 	}
 }
--- src/Simplify.Web/Model/Validation/Attributes/ValidationAttribute.cs
+++ src/Simplify.Web/Model/Validation/Attributes/ValidationAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using Simplify.DI;
+using Simplify.Web.Modules.Data;
 
 namespace Simplify.Web.Model.Validation.Attributes
 {
@@ -12,11 +13,52 @@ namespace Simplify.Web.Model.Validation.Attributes
 	public abstract class ValidationAttribute : Attribute
 	{
 		/// <summary>
+		/// Initializes a new instance of the <see cref="ValidationAttribute" /> class.
+		/// </summary>
+		/// <param name="errorMessage">The custom error message, should contain string table item key if messageFromStringTable is true.</param>
+		/// <param name="isMessageFromStringTable">if set to <c>true</c> then indicates that errorMessage is containing string table item key instead of string error message.</param>
+		protected ValidationAttribute(string errorMessage = null, bool isMessageFromStringTable = true)
+		{
+			ErrorMessage = errorMessage;
+			IsMessageFromStringTable = isMessageFromStringTable;
+		}
+
+		/// <summary>
+		/// Gets the error message.
+		/// </summary>
+		/// <value>
+		/// The error message.
+		/// </value>
+		protected string ErrorMessage { get; }
+
+		/// <summary>
+		/// Gets a value indicating whether the ErrorMessage is a message from string table.
+		/// </summary>
+		protected bool IsMessageFromStringTable { get; }
+
+		/// <summary>
 		/// Validates the specified property value.
 		/// </summary>
 		/// <param name="value">The object value.</param>
 		/// <param name="propertyInfo">Information about the property containing this attribute.</param>
 		/// <param name="resolver">The objects resolver, useful if you need to retrieve some dependencies to perform validation.</param>
 		public abstract void Validate(object value, PropertyInfo propertyInfo, IDIResolver resolver);
+
+		/// <summary>
+		/// Throws the custom or string table exception.
+		/// </summary>
+		/// <param name="resolver">The resolver.</param>
+		/// <exception cref="ModelValidationException">
+		/// </exception>
+		protected void TryThrowCustomOrStringTableException(IDIResolver resolver)
+		{
+			if (string.IsNullOrEmpty(ErrorMessage))
+				return;
+
+			if (IsMessageFromStringTable)
+				throw new ModelValidationException(resolver.Resolve<IStringTable>().GetItem(ErrorMessage));
+
+			throw new ModelValidationException(ErrorMessage);
+		}
 	}
 }
