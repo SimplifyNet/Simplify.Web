7ef1042c4c0da9eebe2fddf51bf55b747762a625
Alexanderius <alexander.krylkov@gmail.com>
2019-10-17 21:57:00 +0600

    RegexAttributeTests refactored to validation attribute

--- src/Simplify.Web/Model/Validation/Attributes/RegexAttribute.cs
+++ src/Simplify.Web/Model/Validation/Attributes/RegexAttribute.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Simplify.DI;
 
 namespace Simplify.Web.Model.Validation.Attributes
 {
@@ -6,13 +9,15 @@ namespace Simplify.Web.Model.Validation.Attributes
 	/// Indicates what this property should match regular expression
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Property)]
-	public class RegexAttribute : Attribute
+	public class RegexAttribute : ValidationAttribute
 	{
 		/// <summary>
-		/// Initializes a new instance of the <see cref="RegexAttribute"/> class.
+		/// Initializes a new instance of the <see cref="RegexAttribute" /> class.
 		/// </summary>
 		/// <param name="regexString">The regex string.</param>
-		public RegexAttribute(string regexString)
+		/// <param name="errorMessage">The error message.</param>
+		/// <param name="isMessageFromStringTable">if set to <c>true</c> [is message from string table].</param>
+		public RegexAttribute(string regexString, string errorMessage = null, bool isMessageFromStringTable = true) : base(errorMessage, isMessageFromStringTable)
 		{
 			RegexString = regexString;
 		}
@@ -24,5 +29,24 @@ namespace Simplify.Web.Model.Validation.Attributes
 		/// The regex string.
 		/// </value>
 		public string RegexString { get; set; }
+
+		/// <summary>
+		/// Validates the specified property value.
+		/// </summary>
+		/// <param name="value">The object value.</param>
+		/// <param name="propertyInfo">Information about the property containing this attribute.</param>
+		/// <param name="resolver">The objects resolver, useful if you need to retrieve some dependencies to perform validation.</param>
+		public override void Validate(object value, PropertyInfo propertyInfo, IDIResolver resolver)
+		{
+			if (!(value is string))
+				return;
+
+			if (Regex.IsMatch((string)value, RegexString))
+				return;
+
+			TryThrowCustomOrStringTableException(resolver);
+
+			throw new ModelValidationException($"Property '{propertyInfo.Name}' regex not matched, actual value: '{value}', pattern: '{RegexString}'");
+		}
 	}
 }
