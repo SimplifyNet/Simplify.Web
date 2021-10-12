597ec5cfacef9fda44a4f642a631029a5e7912dd
Alexanderius <alexander.krylkov@gmail.com>
2021-10-12 23:22:05 +0600

    [add] Initial weather forecasts component

--- src/SampleApps/SampleApp.Angular/ClientApp/src/app/app-routing.module.ts
+++ src/SampleApps/SampleApp.Angular/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherForecastsComponent } from './weather-forecasts/weather-forecasts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'weather-forecasts', component: WeatherForecastsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/SampleApps/SampleApp.Angular/ClientApp/src/app/app.module.ts
+++ src/SampleApps/SampleApp.Angular/ClientApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherForecastsComponent } from './weather-forecasts/weather-forecasts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherForecastsComponent
   ],
   imports: [
     BrowserModule,
