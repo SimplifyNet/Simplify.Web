24f6589533bc26883c8351b08237e52598bd73de
Alexanderius <alexander.krylkov@gmail.com>
2023-07-07 18:52:07 +0600

    [#226] [add] impl

---
+++ src/Simplify.Web/Responses/Created.cs
@@ -0,0 +1,16 @@
+﻿namespace Simplify.Web.Responses;
+
+/// <summary>
+/// Provides Creted controller string response with 201 HTTP Status Code
+/// </summary>
+public class Created : Content
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Created" /> class.
+	/// </summary>
+	/// <param name="content">The string content.</param>
+	/// <param name="contentType">Type of the content.</param>
+	public Created(string content, string contentType = "text/plain") : base(content, 201, contentType)
+	{
+	}
+}
