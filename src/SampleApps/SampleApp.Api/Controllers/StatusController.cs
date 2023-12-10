423ae6877130ed30825f190257f22a85ddc9c66d
Alexanderius <alexander.krylkov@gmail.com>
2023-12-10 22:00:42 +0600

    [#227] [add] status controller

---
+++ src/SampleApps/SampleApp.Api/Controllers/StatusController.cs
@@ -0,0 +1,10 @@
+﻿using Simplify.Web;
+using Simplify.Web.Attributes;
+
+namespace SampleApp.Api.Controllers;
+
+[Get("status")]
+public class StatusController : Controller
+{
+	public override ControllerResponse Invoke() => Content("Service is running!");
+}
