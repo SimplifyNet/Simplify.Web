c4464c039cdbb9e09fb9a9e0aa02376414d31ea2
Alexanderius <alexander.krylkov@gmail.com>
2023-12-12 21:18:55 +0600

    [#227] [add] test controller

---
+++ src/Simplify.Web.Tests/TestEntities/TestControllerV2.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Simplify.Web.Tests.TestEntities;
+
+public class TestControllerV2 : ControllerV2
+{
+	public ControllerResponse Invoke() => throw new NotImplementedException();
+}
