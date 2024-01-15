576170b099ab790bea55c2a22d026247db1a892f
Alexanderius <alexander.krylkov@gmail.com>
2024-01-15 22:49:56 +0600

    [r] formatting and new c# features

--- src/Simplify.Web/Meta/ControllersMetaStore.cs
+++ src/Simplify.Web/Meta/ControllersMetaStore.cs
@@ -8,27 +8,22 @@ namespace Simplify.Web.Meta;
 /// <summary>
 /// Loads and stores controllers meta information
 /// </summary>
-public class ControllersMetaStore : IControllersMetaStore
+/// <remarks>
+/// Initializes a new instance of the <see cref="ControllersMetaStore"/> class.
+/// </remarks>
+/// <param name="metaDataFactory">The meta data factory.</param>
+public class ControllersMetaStore(IControllerMetaDataFactory metaDataFactory) : IControllersMetaStore
 {
 	private static IControllersMetaStore? _current;
-	private readonly IControllerMetaDataFactory _metaDataFactory;
+	private readonly IControllerMetaDataFactory _metaDataFactory = metaDataFactory;
 	private IList<IControllerMetaData>? _controllersMetaData;
-
-	/// <summary>
-	/// Initializes a new instance of the <see cref="ControllersMetaStore"/> class.
-	/// </summary>
-	/// <param name="metaDataFactory">The meta data factory.</param>
-	public ControllersMetaStore(IControllerMetaDataFactory metaDataFactory) => _metaDataFactory = metaDataFactory;
 
 	/// <summary>
 	/// Current controllers meta store
 	/// </summary>
 	public static IControllersMetaStore Current
 	{
-		get
-		{
-			return _current ??= new ControllersMetaStore(new ControllerMetaDataFactory());
-		}
+		get => _current ??= new ControllersMetaStore(new ControllerMetaDataFactory());
 		set => _current = value ?? throw new ArgumentNullException(nameof(value));
 	}
 
@@ -46,14 +41,16 @@ public class ControllersMetaStore : IControllersMetaStore
 			var controllersMetaContainers = new List<IControllerMetaData>();
 
 			var types = SimplifyWebTypesFinder.FindTypesDerivedFrom<Controller>();
+
 			types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom<AsyncController>()).ToList();
 			types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(typeof(Controller<>))).ToList();
 			types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(typeof(AsyncController<>))).ToList();
 
 			var typesToIgnore = new List<Type>();
 
-			var ignoreContainingClass =
-				SimplifyWebTypesFinder.GetAllTypes().FirstOrDefault(t => t.IsDefined(typeof(IgnoreControllersAttribute), true));
+			var ignoreContainingClass = SimplifyWebTypesFinder
+											.GetAllTypes()
+											.FirstOrDefault(t => t.IsDefined(typeof(IgnoreControllersAttribute), true));
 
 			if (ignoreContainingClass != null)
 			{
@@ -71,10 +68,8 @@ public class ControllersMetaStore : IControllersMetaStore
 	private void LoadMetaData(ICollection<IControllerMetaData> controllersMetaContainers, IEnumerable<Type> types, IEnumerable<Type> typesToIgnore)
 	{
 		foreach (var t in types.Where(t => typesToIgnore.All(x => x.FullName != t.FullName) &&
-		                                   controllersMetaContainers.All(x => x.ControllerType != t)))
-		{
+										   controllersMetaContainers.All(x => x.ControllerType != t)))
 			BuildControllerMetaData(controllersMetaContainers, t);
-		}
 	}
 
 	private void BuildControllerMetaData(ICollection<IControllerMetaData> controllersMetaContainers, Type controllerType) =>
