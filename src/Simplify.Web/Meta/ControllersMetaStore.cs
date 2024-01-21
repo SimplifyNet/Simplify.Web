005bf61f3284afe759694158e6d9dfd72e81f4f1
Alexanderius <alexander.krylkov@gmail.com>
2024-01-21 11:14:17 +0600

    [#227] [r] more readable and cleaner  code

--- src/Simplify.Web/Meta/ControllersMetaStore.cs
+++ src/Simplify.Web/Meta/ControllersMetaStore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using Simplify.Web.Attributes.Setup;
 
@@ -38,41 +39,38 @@ public class ControllersMetaStore(IControllerMetaDataFactory metaDataFactory) : 
 			if (_controllersMetaData != null)
 				return _controllersMetaData;
 
-			var controllersMetaContainers = new List<IControllerMetaData>();
-
-			var types = SimplifyWebTypesFinder.FindTypesDerivedFrom<Controller2>();
-			types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(typeof(Controller2<>))).ToList();
-			types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom<Controller>()).ToList();
-			types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom<AsyncController>()).ToList();
-			types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(typeof(Controller<>))).ToList();
-			types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(typeof(AsyncController<>))).ToList();
-
-			var typesToIgnore = new List<Type>();
-
-			var ignoreContainingClass = SimplifyWebTypesFinder
-											.GetAllTypes()
-											.FirstOrDefault(t => t.IsDefined(typeof(IgnoreControllersAttribute), true));
-
-			if (ignoreContainingClass != null)
-			{
-				var attributes = ignoreContainingClass.GetCustomAttributes(typeof(IgnoreControllersAttribute), false);
-
-				typesToIgnore.AddRange(((IgnoreControllersAttribute)attributes[0]).Types);
-			}
-
-			LoadMetaData(controllersMetaContainers, types, typesToIgnore);
-
-			return _controllersMetaData = controllersMetaContainers;
+			return _controllersMetaData = LoadMetaData();
 		}
 	}
 
-	private void LoadMetaData(ICollection<IControllerMetaData> controllersMetaContainers, IEnumerable<Type> types, IEnumerable<Type> typesToIgnore)
+	private IList<IControllerMetaData> LoadMetaData()
 	{
-		foreach (var t in types.Where(t => typesToIgnore.All(x => x.FullName != t.FullName) &&
-										   controllersMetaContainers.All(x => x.ControllerType != t)))
-			BuildControllerMetaData(controllersMetaContainers, t);
+		var types = SimplifyWebTypesFinder.FindTypesDerivedFrom<Controller2>();
+		types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(typeof(Controller2<>))).ToList();
+		types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom<Controller>()).ToList();
+		types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom<AsyncController>()).ToList();
+		types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(typeof(Controller<>))).ToList();
+		types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(typeof(AsyncController<>))).ToList();
+
+		return LoadMetaData(types, GetTypesToIgnore());
 	}
 
-	private void BuildControllerMetaData(ICollection<IControllerMetaData> controllersMetaContainers, Type controllerType) =>
-		controllersMetaContainers.Add(_metaDataFactory.CreateControllerMetaData(controllerType));
+	private static IEnumerable<Type> GetTypesToIgnore()
+	{
+		var ignoreContainingClass = SimplifyWebTypesFinder
+									.GetAllTypes()
+									.FirstOrDefault(t => t.IsDefined(typeof(IgnoreControllersAttribute), true));
+
+		if (ignoreContainingClass == null)
+			return new List<Type>();
+
+		var attributes = ignoreContainingClass.GetCustomAttributes(typeof(IgnoreControllersAttribute), false);
+
+		return ((IgnoreControllersAttribute)attributes[0]).Types;
+	}
+
+	private IList<IControllerMetaData> LoadMetaData(IEnumerable<Type> types, IEnumerable<Type> typesToIgnore) =>
+		types.Where(t => typesToIgnore.All(x => x.FullName != t.FullName))
+			.Select(_metaDataFactory.CreateControllerMetaData)
+			.ToList();
 }
