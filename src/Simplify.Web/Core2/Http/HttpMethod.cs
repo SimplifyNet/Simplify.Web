37805e7e843c9c80766d44f111e20718eb6b49e9
Alexanderius <alexander.krylkov@gmail.com>
2024-04-09 21:17:56 +0500

    [add] HTTP method

---
+++ src/Simplify.Web/Core2/Http/HttpMethod.cs
@@ -0,0 +1,42 @@
+﻿namespace Simplify.Web.Core2.Http;
+
+/// <summary>
+/// Provides HTTP methods used in Simplify.Web.
+/// </summary>
+public enum HttpMethod
+{
+	/// <summary>
+	/// Undefined HTTP method.
+	/// </summary>
+	Undefined,
+
+	/// <summary>
+	/// The GET HTTP method.
+	/// </summary>
+	Get,
+
+	/// <summary>
+	/// The POST HTTP method.
+	/// </summary>
+	Post,
+
+	/// <summary>
+	/// The PUT HTTP method.
+	/// </summary>
+	Put,
+
+	/// <summary>
+	/// The PATCH HTTP method.
+	/// </summary>
+	Patch,
+
+	/// <summary>
+	/// The DELETE HTTP method.
+	/// </summary>
+	Delete,
+
+	/// <summary>
+	/// The OPTIONS HTTP method.
+	/// </summary>
+	Options
+}
