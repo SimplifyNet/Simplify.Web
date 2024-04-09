50f9a6f7cdfc4a18e1340c0bdedae09091cc4c6c
Alexanderius <alexander.krylkov@gmail.com>
2024-04-09 21:44:22 +0500

    [add] meta interfaces

---
+++ src/Simplify.Web/Meta2/IControllersMetaStore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Simplify.Web.Meta2;
+
+/// <summary>
+/// Represent controllers meta store.
+/// </summary>
+public interface IControllersMetaStore
+{
+	/// <summary>
+	/// Current domain controllers metadata
+	/// </summary>
+	/// <returns></returns>
+	IList<IControllerMetaData> ControllersMetaData { get; }
+}
