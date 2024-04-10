192e07312bb23a4829c40c2a487e58c587e48814
Alexanderius <alexander.krylkov@gmail.com>
2024-04-10 22:09:21 +0500

    [del] redundant dependency

--- src/Simplify.Web/Core2/Controllers/Processing/Stages/NotAuthenticatedHandler.cs
+++ src/Simplify.Web/Core2/Controllers/Processing/Stages/NotAuthenticatedHandler.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Simplify.Web.Core.Controllers;
 using Simplify.Web.Core2.Controllers.Processing.Security;
 using Simplify.Web.Modules;
 
 namespace Simplify.Web.Core2.Controllers.Processing.Stages;
 
-public class NotAuthenticatedHandler(IControllersAgent agent, IRedirector redirector) : IControllerProcessingStage
+public class NotAuthenticatedHandler(IRedirector redirector) : IControllerProcessingStage
 {
 	public Task Execute(IControllerProcessingContext args, Action stopProcessing)
 	{
