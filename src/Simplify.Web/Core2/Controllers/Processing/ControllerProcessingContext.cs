660bc7f0d67710bc2e296bf162785f339a39c7c4
Alexanderius <alexander.krylkov@gmail.com>
2024-04-10 22:47:37 +0500

    [edit] ControllerProcessingContext spec update

--- src/Simplify.Web/Core2/Controllers/Processing/ControllerProcessingContext.cs
+++ src/Simplify.Web/Core2/Controllers/Processing/ControllerProcessingContext.cs
@@ -1,19 +1,15 @@
-using System.Collections.Generic;
 using Simplify.Web.Core2.Controllers.Processing.Security;
+using Simplify.Web.Core2.Controllers.Routing;
 using Simplify.Web.Core2.Http;
-using Simplify.Web.Meta;
 
 namespace Simplify.Web.Core2.Controllers.Processing;
 
 public class ControllerProcessingContext(
-	IControllerMetaData controllerMetaData,
+	IMatchedController matchedController,
 	SecurityStatus securityStatus,
-	IHttpContext context,
-	IDictionary<string, object>? routeParameters) : IControllerProcessingContext
+	IHttpContext context) : IControllerProcessingContext
 {
-	public IControllerMetaData ControllerMetaData { get; } = controllerMetaData;
+	public IMatchedController Controller { get; } = matchedController;
 	public SecurityStatus SecurityStatus { get; } = securityStatus;
 	public IHttpContext Context { get; } = context;
-
-	public IDictionary<string, object>? RouteParameters { get; } = routeParameters;
 }
--- src/Simplify.Web/Core2/Controllers/Processing/IControllerProcessingContext.cs
+++ src/Simplify.Web/Core2/Controllers/Processing/IControllerProcessingContext.cs
@@ -1,17 +1,14 @@
-﻿using System.Collections.Generic;
-using Simplify.Web.Core2.Controllers.Processing.Security;
+﻿using Simplify.Web.Core2.Controllers.Processing.Security;
+using Simplify.Web.Core2.Controllers.Routing;
 using Simplify.Web.Core2.Http;
-using Simplify.Web.Meta;
 
 namespace Simplify.Web.Core2.Controllers.Processing;
 
 public interface IControllerProcessingContext
 {
-	public IControllerMetaData ControllerMetaData { get; }
+	public IMatchedController Controller { get; }
 
 	public SecurityStatus SecurityStatus { get; }
 
 	public IHttpContext Context { get; }
-
-	public IDictionary<string, object>? RouteParameters { get; }
 }
