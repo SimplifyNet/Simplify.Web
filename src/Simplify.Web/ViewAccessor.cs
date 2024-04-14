a945c99da9d801701e62cc25f4ae1c991e3ce035
Alexanderius <alexander.krylkov@gmail.com>
2024-04-14 21:23:45 +0500

    [edit] views revisited

---
+++ src/Simplify.Web/ViewAccessor.cs
@@ -0,0 +1,20 @@
+﻿using Simplify.Web.Old;
+using Simplify.Web.Views;
+
+namespace Simplify.Web;
+
+/// <summary>
+/// Provides View accessor base class.
+/// </summary>
+public abstract class ViewAccessor
+{
+	internal IViewFactory ViewFactory { get; set; } = null!;
+
+	/// <summary>
+	/// Gets a view instance.
+	/// </summary>
+	/// <typeparam name="T">View instance to get</typeparam>
+	public T GetView<T>()
+		where T : View =>
+		(T)ViewFactory.CreateView(typeof(T));
+}
---
+++ src/Simplify.Web/Views/IViewFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Simplify.Web.Old;
+
+namespace Simplify.Web.Views;
+
+/// <summary>
+/// Represents a view factory.
+/// </summary>
+public interface IViewFactory
+{
+	/// <summary>
+	/// Creates the view.
+	/// </summary>
+	/// <param name="viewType">Type of the view.</param>
+	View CreateView(Type viewType);
+}
