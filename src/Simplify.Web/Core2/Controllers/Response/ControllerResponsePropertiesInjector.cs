a71bc62554503a78f08a52685f7f92fb01eba82d
Alexanderius <alexander.krylkov@gmail.com>
2024-04-14 11:37:10 +0500

    [r] naming

---
+++ src/Simplify.Web/Core2/Controllers/Response/ControllerResponsePropertiesInjector.cs
@@ -0,0 +1,25 @@
+﻿using Simplify.DI;
+using Simplify.Web.Core;
+using Simplify.Web.Core2.Controllers.Response.Injectors;
+
+namespace Simplify.Web.Core2.Controllers.Response;
+
+
+/// <summary>
+/// Provides controller response builder.
+/// </summary>
+public class ControllerResponsePropertiesInjector(IDIResolver resolver) : ActionModulesAccessorInjector(resolver), IControllerResponsePropertiesInjector
+{
+	private readonly IDIResolver _resolver = resolver;
+
+	/// <summary>
+	/// Builds the controller response properties.
+	/// </summary>
+	/// <param name="controllerResponse">The controller response.</param>
+	public void Inject(ControllerResponse controllerResponse)
+	{
+		InjectActionModulesAccessorProperties(controllerResponse);
+
+		controllerResponse.ResponseWriter = _resolver.Resolve<IResponseWriter>();
+	}
+}
