e2af23e138d35ce5de5af1057dab2a4776cb92e5
Alexanderius <alexander.krylkov@gmail.com>
2024-04-15 22:43:54 +0500

    [edit] revisited

--- src/Simplify.Web.Tests/Old/Core/Controllers/Execution/Controller2ExecutorTestTypes/StringResponseController.cs
+++ src/Simplify.Web.Tests/Old/Core/Controllers/Execution/Controller2ExecutorTestTypes/StringResponseController.cs
@@ -1,8 +1,6 @@
-using Simplify.Web.Old;
-
 namespace Simplify.Web.Tests.Old.Core.Controllers.Execution.Controller2ExecutorTestTypes;
 
-public class StringResponseController : Controller2
+public class StringResponseController : Web.Old.Controller2
 {
 	public virtual Web.Old.ControllerResponse Invoke() => Content("Test!");
 }
---
+++ src/Simplify.Web/Controller2.cs
@@ -0,0 +1,8 @@
+﻿namespace Simplify.Web;
+
+/// <summary>
+/// Provides the user controllers base class version 2.
+/// </summary>
+public abstract class Controller2 : Controller2Base
+{
+}
