83f5553702e3389cb279300b99a6f2da8dc6dfcd
Alexanderius <alexander.krylkov@gmail.com>
2024-04-18 19:44:55 +0500

    [edit] revisited

---
+++ src/Simplify.Web/Meta/IController1Metadata.cs
@@ -0,0 +1,6 @@
+﻿namespace Simplify.Web.Meta;
+
+/// <summary>
+/// Represent a controller v1 metadata information.
+/// </summary>
+public interface IController1Metadata : IControllerMetadata;
---
+++ src/Simplify.Web/Meta/IController2Metadata.cs
@@ -0,0 +1,6 @@
+﻿namespace Simplify.Web.Meta;
+
+/// <summary>
+/// Represent a controller v2 metadata information.
+/// </summary>
+public interface IController2Metadata : IControllerMetadata;
