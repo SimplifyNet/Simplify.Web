038a43fc4c729ed51018e74504e6e66b536bc277
Alexanderius <alexander.krylkov@gmail.com>
2024-05-05 14:48:02 +0500

    [r] namespace

--- src/Simplify.Web/Controllers/V2/Metadata/IController2Metadata.cs
+++ src/Simplify.Web/Controllers/V2/Metadata/IController2Metadata.cs
@@ -1,4 +1,6 @@
-﻿namespace Simplify.Web.Meta.Controllers.V2;
+﻿using Simplify.Web.Meta.Controllers;
+
+namespace Simplify.Web.Controllers.V2.Metadata;
 
 /// <summary>
 /// Represent a controller v2 metadata information.
