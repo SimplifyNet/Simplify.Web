e6773b0f414a8c4efa32a3d3091994f1f0d8a3e6
Alexanderius <alexander.krylkov@gmail.com>
2024-05-09 16:51:16 +0500

    [add] Controller2Types

--- src/Simplify.Web/Controllers/Meta/Loader/MetadataLoader.cs
+++ src/Simplify.Web/Controllers/Meta/Loader/MetadataLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Simplify.Web.Controllers.Meta.Factory;
 using Simplify.Web.Controllers.V1.Metadata;
+using Simplify.Web.Controllers.V2.Metadata;
 using Simplify.Web.System;
 
 namespace Simplify.Web.Controllers.Meta.Loader;
@@ -16,18 +17,17 @@ public class MetadataLoader(IControllerMetadataFactoryResolver resolver) : IMeta
 	/// </summary>
 	public static IMetadataLoader Current
 	{
-		get => _loader ??= new MetadataLoader(new ControllerMetadataFactoryResolver(new List<IControllerMetadataFactory>{
-				new Controller1MetadataFactory()
-			}));
+		get => _loader ??= new MetadataLoader(new ControllerMetadataFactoryResolver(
+		[
+			new Controller1MetadataFactory()
+		]));
 
 		set => _loader = value ?? throw new ArgumentNullException(nameof(value));
 	}
 
 	public IReadOnlyCollection<IControllerMetadata> Load()
 	{
-		var types = SimplifyWebTypesFinder.FindTypesDerivedFrom<Controller2>();
-
-		types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(typeof(Controller2<>))).ToList();
+		var types = SimplifyWebTypesFinder.FindTypesDerivedFrom(Controller2Types.Types);
 		types = types.Concat(SimplifyWebTypesFinder.FindTypesDerivedFrom(Controller1Types.Types)).ToList();
 
 		return LoadMetadata(types, SimplifyWebTypesFinder.GetControllerTypesToIgnore());
---
+++ src/Simplify.Web/Controllers/V2/Metadata/Controller2Types.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Simplify.Web.Controllers.V2.Metadata;
+
+public static class Controller2Types
+{
+	public static Type[] Types =>
+	[
+		typeof(Controller2),
+		typeof(Controller2<>),
+	];
+}
