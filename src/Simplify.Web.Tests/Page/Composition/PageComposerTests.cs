c368d069f0fff333366f7d907f14f9f442b0cda3
Alexanderius <alexander.krylkov@gmail.com>
2024-05-11 22:15:22 +0500

    [add] PageGeneratorTests, PageComposerTests [del] refactored

---
+++ src/Simplify.Web.Tests/Page/Composition/PageComposerTests.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using NUnit.Framework;
+using Simplify.Web.Modules.Data;
+using Simplify.Web.Page.Composition;
+using Simplify.Web.Page.Generation;
+
+namespace Simplify.Web.Tests.Page.Composition;
+
+[TestFixture]
+public class PageComposerTests
+{
+	private PageComposer _pageComposer = null!;
+
+	private Mock<IDataCollector> _dataCollector = null!;
+	private Mock<IPageGenerator> _pageGenerator = null!;
+
+	private Mock<IPageCompositionStage> _stage1 = null!;
+	private Mock<IPageCompositionStage> _stage2 = null!;
+
+	[SetUp]
+	public void Initialize()
+	{
+		_dataCollector = new Mock<IDataCollector>();
+		_pageGenerator = new Mock<IPageGenerator>();
+		_stage1 = new Mock<IPageCompositionStage>();
+		_stage2 = new Mock<IPageCompositionStage>();
+
+		_pageComposer = new PageComposer([_stage1.Object, _stage2.Object], _dataCollector.Object, _pageGenerator.Object);
+	}
+
+	[Test]
+	public void Compose_StagesExecutedAndPageReturned()
+	{
+		// Arrange
+
+		_pageGenerator.Setup(x => x.Generate(It.Is<IDataCollector>(dc => dc == _dataCollector.Object))).Returns("Foo");
+
+		// Act
+		var result = _pageComposer.Compose();
+
+		// Assert
+
+		Assert.That(result, Is.EqualTo("Foo"));
+
+		_stage1.Verify(x => x.Execute(It.Is<IDataCollector>(dc => dc == _dataCollector.Object)));
+		_stage2.Verify(x => x.Execute(It.Is<IDataCollector>(dc => dc == _dataCollector.Object)));
+	}
+}
---
+++ src/Simplify.Web.Tests/Page/Generation/PageGeneratorTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualBasic;
+using Moq;
+using NUnit.Framework;
+using Simplify.Templates;
+using Simplify.Web.Modules.ApplicationEnvironment;
+using Simplify.Web.Modules.Data;
+using Simplify.Web.Page.Generation;
+
+namespace Simplify.Web.Tests.Page.Generation;
+
+[TestFixture]
+public class PageGeneratorTests
+{
+	private PageGenerator _pageGenerator = null!;
+
+	private Mock<ITemplateFactory> _templateFactory = null!;
+
+	[SetUp]
+	public void Initialize() => _templateFactory = new Mock<ITemplateFactory>();
+
+	[Test]
+	public void Generate_TemplateLoadedAndDataCollectorVariablesInjected()
+	{
+		// Arrange
+
+		var environment = Mock.Of<IDynamicEnvironment>(x => x.MasterTemplateFileName == "Bar");
+		var tpl = TemplateBuilder.FromString("Foo{Var}").Build();
+		var dataCollector = Mock.Of<IDataCollector>(x => x.Items == new Dictionary<string, string> { { "Var", "Val" } });
+
+		_templateFactory.Setup(x => x.Load(It.Is<string>(s => s == "Bar"))).Returns(tpl);
+
+		_pageGenerator = new PageGenerator(_templateFactory.Object, environment);
+
+		// Act
+		var result = _pageGenerator.Generate(dataCollector);
+
+		// Assert
+		Assert.That(result, Is.EqualTo("FooVal"));
+	}
+}
