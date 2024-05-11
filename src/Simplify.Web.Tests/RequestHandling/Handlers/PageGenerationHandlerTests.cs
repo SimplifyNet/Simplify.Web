2ac1d48c430c36cbd75fd7c0973fac5db92eaa17
Alexanderius <alexander.krylkov@gmail.com>
2024-05-11 21:34:03 +0500

    [add] PageGenerationHandlerTests

---
+++ src/Simplify.Web.Tests/RequestHandling/Handlers/PageGenerationHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NUnit.Framework;
+using Simplify.Web.Http.ResponseWriting;
+using Simplify.Web.Page.Composition;
+using Simplify.Web.RequestHandling.Handlers;
+
+namespace Simplify.Web.Tests.Old.Core.PageAssembly;
+
+[TestFixture]
+public class PageGenerationHandlerTests
+{
+	private PageGenerationHandler _handler = null!;
+
+	private Mock<IPageComposer> _pageComposer = null!;
+	private Mock<IResponseWriter> _responseWriter = null!;
+
+	[SetUp]
+	public void Initialize()
+	{
+		_pageComposer = new Mock<IPageComposer>();
+		_responseWriter = new Mock<IResponseWriter>();
+
+		_handler = new PageGenerationHandler(_pageComposer.Object, _responseWriter.Object);
+	}
+
+	[Test]
+	public async Task HandleAsync_ResponseWriterCalledWithDataFromComposeAndRespectiveResponseFieldsAreSet()
+	{
+		// Arrange
+
+		_pageComposer.Setup(x => x.Compose()).Returns("Foo");
+
+		var response = Mock.Of<HttpResponse>();
+
+		var httpContext = Mock.Of<HttpContext>(x =>
+			x.Request.Path == new PathString("/foo") &&
+			x.Response == response);
+
+		// Act
+		await _handler.HandleAsync(httpContext, null!);
+
+		// Assert
+
+		Assert.That(response.ContentType, Is.EqualTo("text/html"));
+
+		_responseWriter.Verify(x => x.WriteAsync(It.Is<HttpResponse>(r => r == response), It.Is<string>(s => s == "Foo")));
+	}
+}
