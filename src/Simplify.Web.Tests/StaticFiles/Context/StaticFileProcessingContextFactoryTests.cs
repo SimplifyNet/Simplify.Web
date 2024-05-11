c0511442a54f3a5a31589e03cf08a8da0bdeaec5
Alexanderius <alexander.krylkov@gmail.com>
2024-05-11 19:32:41 +0500

    [add] StaticFileProcessingContextFactoryTests

---
+++ src/Simplify.Web.Tests/StaticFiles/Context/StaticFileProcessingContextFactoryTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NUnit.Framework;
+using Simplify.Web.StaticFiles.Context;
+using Simplify.Web.StaticFiles.IO;
+
+namespace Simplify.Web.Tests.StaticFiles.Context;
+
+[TestFixture]
+public class StaticFileProcessingContextFactoryTests
+{
+	private Mock<IStaticFile> _file = null!;
+	private StaticFileProcessingContextFactory _factory = null!;
+
+	[SetUp]
+	public void Initialize()
+	{
+		_file = new Mock<IStaticFile>();
+		_factory = new StaticFileProcessingContextFactory(_file.Object);
+	}
+
+	[Test]
+	public void Create_SomeValuesAndCantBeCached_CopiedToContext()
+	{
+		// Arrange
+
+		var filePath = "foo";
+		var lastModificationTime = new DateTime(2023, 5, 2, 15, 14, 0);
+		var httpContext = Mock.Of<HttpContext>(x => x.Request == Mock.Of<HttpRequest>(r => r.Headers == new HeaderDictionary()));
+
+		_file.Setup(x => x.GetLastModificationTime(It.Is<string>(x => x == filePath))).Returns(lastModificationTime);
+
+		// Act
+		var context = _factory.Create(httpContext, filePath);
+
+		// Assert
+
+		Assert.That(context.RelativeFilePath, Is.EqualTo(filePath));
+		Assert.That(context.LastModificationTime, Is.EqualTo(lastModificationTime));
+		Assert.That(context.CanBeCached, Is.False);
+	}
+}
