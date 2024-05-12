e3e8cba8c6bbeeb6bb83a37d13a43b02b638d4e9
Alexanderius <alexander.krylkov@gmail.com>
2024-05-12 14:00:35 +0500

    [r] Environment

---
+++ src/Simplify.Web.Tests/Modules/ApplicationEnvironment/DynamicEnvironmentTests.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using NUnit.Framework;
+using Simplify.Web.Modules.ApplicationEnvironment;
+using Simplify.Web.Settings;
+
+namespace Simplify.Web.Tests.Modules.ApplicationEnvironment;
+
+[TestFixture]
+public class DynamicEnvironmentTests
+{
+	private ISimplifyWebSettings _settings = null!;
+
+	[SetUp]
+	public void Initialize()
+	{
+		var settings = new Mock<ISimplifyWebSettings>();
+
+		settings.SetupGet(x => x.DefaultTemplatesPath).Returns("Templates");
+		settings.SetupGet(x => x.DefaultStyle).Returns("Main");
+		settings.SetupGet(x => x.DefaultMasterTemplateFileName).Returns("Master.tpl");
+		settings.SetupGet(x => x.DataPath).Returns("App_Data");
+
+		_settings = settings.Object;
+	}
+
+	[Test]
+	public void Constructor_DefaultParameters_PropertiesSetCorrectly()
+	{
+		// Act
+		var env = new DynamicEnvironment(new Environment("C:/Test", _settings), _settings);
+
+		// Assert
+
+		Assert.That(env.SiteStyle, Is.EqualTo("Main"));
+		Assert.That(env.TemplatesPath, Is.EqualTo("Templates"));
+		Assert.That(env.MasterTemplateFileName, Is.EqualTo("Master.tpl"));
+		Assert.That(env.TemplatesPath, Is.EqualTo("Templates"));
+		Assert.That(env.TemplatesPhysicalPath, Is.EqualTo("C:/Test/Templates/"));
+	}
+
+	[Test]
+	public void Constructor_BackslashPath_BackslashAdded()
+	{
+		// Act
+		var env = new DynamicEnvironment(new Environment(@"C:\Test\", _settings), _settings);
+
+		// Assert
+		Assert.That(env.TemplatesPhysicalPath, Is.EqualTo("C:/Test/Templates/"));
+	}
+}
---
+++ src/Simplify.Web.Tests/Modules/ApplicationEnvironment/EnvironmentTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using Simplify.Web.Modules.ApplicationEnvironment;
+using Simplify.Web.Settings;
+
+namespace Simplify.Web.Tests.Modules.ApplicationEnvironment;
+
+[TestFixture]
+public class EnvironmentTests
+{
+	private ISimplifyWebSettings _settings = null!;
+
+	[SetUp]
+	public void Initialize()
+	{
+		var settings = new Mock<ISimplifyWebSettings>();
+
+		settings.SetupGet(x => x.DefaultTemplatesPath).Returns("Templates");
+		settings.SetupGet(x => x.DefaultStyle).Returns("Main");
+		settings.SetupGet(x => x.DefaultMasterTemplateFileName).Returns("Master.tpl");
+		settings.SetupGet(x => x.DataPath).Returns("App_Data");
+
+		_settings = settings.Object;
+	}
+
+	[Test]
+	public void Constructor_DefaultParameters_PropertiesSetCorrectly()
+	{
+		// Act
+		var env = new Environment("C:/Test", _settings);
+
+		// Assert
+
+		Assert.That(env.DataPath, Is.EqualTo("App_Data"));
+		Assert.That(env.DataPhysicalPath, Is.EqualTo("C:/Test/App_Data/"));
+	}
+}
