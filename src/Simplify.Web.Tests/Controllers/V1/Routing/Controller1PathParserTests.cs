13b17bf5c66eee21f80d21701600e7941c5ef101
Alexanderius <alexander.krylkov@gmail.com>
2024-05-19 13:40:32 +0500

    [r] ordering

--- src/Simplify.Web.Tests/Controllers/V1/Routing/Controller1PathParserTests.cs
+++ src/Simplify.Web.Tests/Controllers/V1/Routing/Controller1PathParserTests.cs
@@ -9,7 +9,7 @@ namespace Simplify.Web.Tests.Controllers.V1.Routing;
 public class Controller1PathParserTests
 {
 	[Test]
-	public void Parse_Root_NoSegments()
+	public void Parse_RootPath_NoSegments()
 	{
 		// Act
 		var result = Controller1PathParser.Parse("/");
@@ -32,23 +32,6 @@ public class Controller1PathParserTests
 	}
 
 	[Test]
-	public void Parse_MultipleSegments_MultipleSegments()
-	{
-		// Act
-		var result = Controller1PathParser.Parse("/foo/bar/test");
-
-		// Assert
-
-		Assert.That(result.Count, Is.EqualTo(3));
-		Assert.That(result[0] as PathSegment, Is.Not.Null);
-		Assert.That(result[0].Name, Is.EqualTo("foo"));
-		Assert.That(result[1] as PathSegment, Is.Not.Null);
-		Assert.That(result[1].Name, Is.EqualTo("bar"));
-		Assert.That(result[2] as PathSegment, Is.Not.Null);
-		Assert.That(result[2].Name, Is.EqualTo("test"));
-	}
-
-	[Test]
 	public void Parse_OneSegmentOneParameter_OneSegmentOneParameter()
 	{
 		// Act
@@ -57,6 +40,7 @@ public class Controller1PathParserTests
 		// Assert
 
 		Assert.That(result.Count, Is.EqualTo(2));
+
 		Assert.That(result[0] as PathSegment, Is.Not.Null);
 		Assert.That(result[0].Name, Is.EqualTo("foo"));
 
@@ -86,28 +70,24 @@ public class Controller1PathParserTests
 		Assert.That(((PathParameter)result[2]).Type, Is.EqualTo(typeof(int)));
 	}
 
-	[TestCase("/foo/{id:int")]
-	[TestCase("/foo/id:int}")]
-	[TestCase("/foo/:")]
-	[TestCase("/foo/{{")]
-	[TestCase("/foo/}}")]
-	[TestCase("/foo/:}")]
-	[TestCase("/foo/{:")]
-	[TestCase("/foo/{}")]
-	[TestCase("/foo/{:}")]
-	[TestCase("/foo/{{a:int}}")]
-	[TestCase("/foo/]{a:int{")]
-	[TestCase("/foo/]{@#$32127!&}")]
-	public void Parse_BadParameters_ExceptionThrown(string path) =>
-		// Act & Assert
-		Assert.Throws<ControllerRouteException>(() => Controller1PathParser.Parse(path));
-
-	[Test]
-	public void Parse_UnrecognizedParameterType_ExceptionThrown()
+	[TestCase("id", "/{id:decimal}", typeof(decimal))]
+	[TestCase("foo", "/{foo:bool}", typeof(bool))]
+	[TestCase("stringArray", "/{stringArray:[]}", typeof(string[]))]
+	[TestCase("stringArray", "/{stringArray:string[]}", typeof(string[]))]
+	[TestCase("intArray", "/{intArray:int[]}", typeof(int[]))]
+	[TestCase("decimalArray", "/{decimalArray:decimal[]}", typeof(decimal[]))]
+	[TestCase("boolArray", "/{boolArray:bool[]}", typeof(bool[]))]
+	public void Parse_OneParameter_Parsed(string name, string controllerPath, Type type)
 	{
-		// Act & Assert
+		// Act
+		var result = Controller1PathParser.Parse(controllerPath);
 
-		Assert.Throws<ControllerRouteException>(() => Controller1PathParser.Parse("/foo/{id:foo}"));
+		// Assert
+
+		Assert.That(result.Count, Is.EqualTo(1));
+		Assert.That(result[0] as PathParameter, Is.Not.Null);
+		Assert.That(result[0].Name, Is.EqualTo(name));
+		Assert.That(((PathParameter)result[0]).Type, Is.EqualTo(type));
 	}
 
 	[Test]
@@ -130,23 +110,44 @@ public class Controller1PathParserTests
 		Assert.That(result[2].Name, Is.EqualTo("bar"));
 	}
 
-	[TestCase("id", "/{id:decimal}", typeof(decimal))]
-	[TestCase("foo", "/{foo:bool}", typeof(bool))]
-	[TestCase("stringArray", "/{stringArray:[]}", typeof(string[]))]
-	[TestCase("stringArray", "/{stringArray:string[]}", typeof(string[]))]
-	[TestCase("intArray", "/{intArray:int[]}", typeof(int[]))]
-	[TestCase("decimalArray", "/{decimalArray:decimal[]}", typeof(decimal[]))]
-	[TestCase("boolArray", "/{boolArray:bool[]}", typeof(bool[]))]
-	public void Parse_Parameter_Parsed(string name, string controllerPath, Type type)
+	[Test]
+	public void Parse_MultipleSegments_MultipleSegments()
 	{
 		// Act
-		var result = Controller1PathParser.Parse(controllerPath);
+		var result = Controller1PathParser.Parse("/foo/bar/test");
 
 		// Assert
 
-		Assert.That(result.Count, Is.EqualTo(1));
-		Assert.That(result[0] as PathParameter, Is.Not.Null);
-		Assert.That(result[0].Name, Is.EqualTo(name));
-		Assert.That(((PathParameter)result[0]).Type, Is.EqualTo(type));
+		Assert.That(result.Count, Is.EqualTo(3));
+		Assert.That(result[0] as PathSegment, Is.Not.Null);
+		Assert.That(result[0].Name, Is.EqualTo("foo"));
+		Assert.That(result[1] as PathSegment, Is.Not.Null);
+		Assert.That(result[1].Name, Is.EqualTo("bar"));
+		Assert.That(result[2] as PathSegment, Is.Not.Null);
+		Assert.That(result[2].Name, Is.EqualTo("test"));
+	}
+
+	[TestCase("/foo/{id:int")]
+	[TestCase("/foo/id:int}")]
+	[TestCase("/foo/:")]
+	[TestCase("/foo/{{")]
+	[TestCase("/foo/}}")]
+	[TestCase("/foo/:}")]
+	[TestCase("/foo/{:")]
+	[TestCase("/foo/{}")]
+	[TestCase("/foo/{:}")]
+	[TestCase("/foo/{{a:int}}")]
+	[TestCase("/foo/]{a:int{")]
+	[TestCase("/foo/]{@#$32127!&}")]
+	public void Parse_BadParameter_ExceptionThrown(string path) =>
+		// Act & Assert
+		Assert.Throws<ControllerRouteException>(() => Controller1PathParser.Parse(path));
+
+	[Test]
+	public void Parse_UnrecognizedParameterType_ExceptionThrown()
+	{
+		// Act & Assert
+
+		Assert.Throws<ControllerRouteException>(() => Controller1PathParser.Parse("/foo/{id:foo}"));
 	}
 }
