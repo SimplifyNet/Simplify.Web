aeed3fde7e0632ae7feb3532d0b32023fc560579
Alexanderius <alexander.krylkov@gmail.com>
2024-05-19 22:14:53 +0500

    [add] string converter

---
+++ src/Simplify.Web.Tests/System/StringConverterTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using Simplify.Web.System;
+
+namespace Simplify.Web.Controllers.V1.Routing;
+
+[TestFixture]
+public class StringConverterTests
+{
+	[TestCase(typeof(string), "foo", "foo")]
+	[TestCase(typeof(int), "1", 1)]
+	[TestCase(typeof(long), "1", 1L)]
+	[TestCase(typeof(float), "1", 1f)]
+	[TestCase(typeof(double), "1", 1d)]
+	[TestCase(typeof(bool), "true", true)]
+	public void Convert_SpecifiedValue_ConvertedToDestinationType(Type destinationType, string sourceValue, object expectedValue)
+	{
+		// Act
+		var result = StringConverter.TryConvert(destinationType, sourceValue);
+
+		// Assert
+
+		Assert.That(result, Is.Not.Null);
+		Assert.That(result, Is.EqualTo(expectedValue));
+	}
+
+	[Test]
+	public void Convert_Decimal_Converted() =>
+		Convert_SpecifiedValue_ConvertedToDestinationType(typeof(decimal), "1", 1m);
+
+	[Test]
+	public void Convert_StringArray_Converted() =>
+		Convert_SpecifiedValue_ConvertedToDestinationType(typeof(string[]), "a,b,c", new string[] { "a", "b", "c" });
+
+	[Test]
+	public void Convert_IntArray_Converted() =>
+		Convert_SpecifiedValue_ConvertedToDestinationType(typeof(int[]), "1,2,3", new int[] { 1, 2, 3 });
+
+	[Test]
+	public void Convert_DecimalArray_Converted() =>
+		Convert_SpecifiedValue_ConvertedToDestinationType(typeof(decimal[]), "1,2,3", new decimal[] { 1m, 2m, 3m });
+
+	[Test]
+	public void Convert_BoolArray_Converted() =>
+		Convert_SpecifiedValue_ConvertedToDestinationType(typeof(bool[]), "false,true,false", new bool[] { false, true, false });
+}
---
+++ src/Simplify.Web/System/StringConverter.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simplify.Web.System;
+
+public static class StringConverter
+{
+	private static readonly Dictionary<Type, Func<string, object?>> ValueConverters =
+		new()
+		{
+			{ typeof(string), sourceValue => sourceValue },
+			{ typeof(int), GetIntParameterValue },
+			{ typeof(long), GetLongParameterValue },
+			{ typeof(decimal), GetDecimalParameterValue },
+			{ typeof(float), GetFloatParameterValue },
+			{ typeof(double), GetDoubleParameterValue },
+			{ typeof(bool), GetBoolParameterValue },
+			{ typeof(string[]), GetStringArrayParameterValue },
+			{ typeof(int[]), GetIntArrayParameterValue },
+			{ typeof(decimal[]), GetDecimalArrayParameterValue },
+			{ typeof(bool[]), GetBoolArrayParameterValue }
+		};
+
+	public static object? TryConvert(Type destinationType, string sourceValue) =>
+		ValueConverters.TryGetValue(destinationType, out var converter)
+			? converter(sourceValue)
+			: null;
+
+	private static object? GetIntParameterValue(string source)
+	{
+		if (!int.TryParse(source, out var buffer))
+			return null;
+
+		return buffer;
+	}
+
+	private static object? GetLongParameterValue(string source)
+	{
+		if (!long.TryParse(source, out var buffer))
+			return null;
+
+		return buffer;
+	}
+
+	private static object? GetDecimalParameterValue(string source)
+	{
+		if (!decimal.TryParse(source, out var buffer))
+			return null;
+
+		return buffer;
+	}
+
+	private static object? GetFloatParameterValue(string source)
+	{
+		if (!float.TryParse(source, out var buffer))
+			return null;
+
+		return buffer;
+	}
+
+	private static object? GetDoubleParameterValue(string source)
+	{
+		if (!double.TryParse(source, out var buffer))
+			return null;
+
+		return buffer;
+	}
+
+	private static object? GetBoolParameterValue(string source)
+	{
+		if (!bool.TryParse(source, out var buffer))
+			return null;
+
+		return buffer;
+	}
+
+	private static IList<string> GetStringArrayParameterValue(string source) =>
+		source.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+	private static IList<int> GetIntArrayParameterValue(string source) =>
+		source.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+			.Select(GetIntParameterValue)
+			.Where(x => x != null)
+			.Cast<int>()
+			.ToList();
+
+	private static IList<decimal> GetDecimalArrayParameterValue(string source) =>
+		source.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+			.Select(GetDecimalParameterValue)
+			.Where(x => x != null)
+			.Cast<decimal>()
+			.ToList();
+
+	private static IList<bool> GetBoolArrayParameterValue(string source) =>
+		source.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+			.Select(GetBoolParameterValue)
+			.Where(x => x != null)
+			.Cast<bool>()
+			.ToList();
+}
