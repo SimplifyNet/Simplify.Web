97488e2b047d941d3c6c72d258e446681f602de3
Alexanderius <alexander.krylkov@gmail.com>
2024-05-19 22:15:28 +0500

    [r] to string converter

--- src/Simplify.Web/Controllers/V1/Routing/Controller1RouteMatcher.cs
+++ src/Simplify.Web/Controllers/V1/Routing/Controller1RouteMatcher.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Simplify.Web.Controllers.Meta;
 using Simplify.Web.Controllers.Meta.Routing;
 using Simplify.Web.Controllers.RouteMatching;
 using Simplify.Web.Controllers.V1.Metadata;
+using Simplify.Web.System;
 
 namespace Simplify.Web.Controllers.V1.Routing;
 
 public class Controller1RouteMatcher : IRouteMatcher
 {
-	private static readonly Dictionary<Type, Func<string, object?>> ParameterValueConverters =
-		new()
-		{
-			{ typeof(string), sourceValue => sourceValue },
-			{ typeof(int), GetIntParameterValue },
-			{ typeof(decimal), GetDecimalParameterValue },
-			{ typeof(bool), GetBoolParameterValue },
-			{ typeof(string[]), GetStringArrayParameterValue },
-			{ typeof(int[]), GetIntArrayParameterValue },
-			{ typeof(decimal[]), GetDecimalArrayParameterValue },
-			{ typeof(bool[]), GetBoolArrayParameterValue },
-		};
-
 	public bool CanHandle(IControllerMetadata controller) => controller is IController1Metadata;
 
 	public IRouteMatchResult Match(IList<string> currentPath, IControllerRoute controllerRoute)
@@ -52,7 +39,7 @@ public class Controller1RouteMatcher : IRouteMatcher
 				return segment.Name == currentPathSegment;
 
 			case PathParameter parameter:
-				var value = GetParameterValue(parameter, currentPathSegment);
+				var value = StringConverter.TryConvert(parameter.Type, currentPathSegment);
 
 				if (value == null)
 					return false;
@@ -64,57 +51,4 @@ public class Controller1RouteMatcher : IRouteMatcher
 				return false;
 		}
 	}
-
-	private static object? GetParameterValue(PathParameter pathParameter, string sourceValue) =>
-		ParameterValueConverters.TryGetValue(pathParameter.Type, out var converter)
-			? converter(sourceValue)
-			: null;
-
-	private static object? GetIntParameterValue(string source)
-	{
-		if (!int.TryParse(source, out var buffer))
-			return null;
-
-		return buffer;
-	}
-
-	private static object? GetDecimalParameterValue(string source)
-	{
-		if (!decimal.TryParse(source, out var buffer))
-			return null;
-
-		return buffer;
-	}
-
-	private static object? GetBoolParameterValue(string source)
-	{
-		if (!bool.TryParse(source, out var buffer))
-			return null;
-
-		return buffer;
-	}
-
-	private static IList<string> GetStringArrayParameterValue(string source) =>
-		source.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-
-	private static IList<int> GetIntArrayParameterValue(string source) =>
-		source.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
-			.Select(GetIntParameterValue)
-			.Where(x => x != null)
-			.Cast<int>()
-			.ToList();
-
-	private static IList<decimal> GetDecimalArrayParameterValue(string source) =>
-		source.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
-			.Select(GetDecimalParameterValue)
-			.Where(x => x != null)
-			.Cast<decimal>()
-			.ToList();
-
-	private static IList<bool> GetBoolArrayParameterValue(string source) =>
-		source.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
-			.Select(GetBoolParameterValue)
-			.Where(x => x != null)
-			.Cast<bool>()
-			.ToList();
 }
