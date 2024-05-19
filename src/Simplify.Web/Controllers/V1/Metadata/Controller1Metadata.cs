15554c943f7b252979217264a22548c81b0e0f64
Alexanderius <alexander.krylkov@gmail.com>
2024-05-19 10:30:18 +0500

    [r] expression-body method

--- src/Simplify.Web/Controllers/V1/Metadata/Controller1Metadata.cs
+++ src/Simplify.Web/Controllers/V1/Metadata/Controller1Metadata.cs
@@ -7,10 +7,8 @@ namespace Simplify.Web.Controllers.V1.Metadata;
 
 public class Controller1Metadata : ControllerMetadata, IController1Metadata
 {
-	public Controller1Metadata(Type controllerType) : base(controllerType)
-	{
+	public Controller1Metadata(Type controllerType) : base(controllerType) =>
 		ExecParameters = BuildControllerExecParameters(controllerType);
-	}
 
 	protected override IControllerRoute BuildControllerRoute(string path) => new Controller1Route(path);
 }
