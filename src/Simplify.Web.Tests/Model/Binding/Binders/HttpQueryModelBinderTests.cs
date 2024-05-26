b62012e7d0815735260d0a1b83aecac3fb9796d5
Alexanderius <alexander.krylkov@gmail.com>
2024-05-26 10:55:44 +0500

    [add] HttpQueryModelBinderTests

---
+++ src/Simplify.Web.Tests/Model/Binding/Binders/HttpQueryModelBinderTests.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using NUnit.Framework;
+using Simplify.Web.Model.Binding;
+using Simplify.Web.Model.Binding.Binders;
+using Simplify.Web.Modules.Context;
+using Simplify.Web.Tests.Model.Binding.Binders.TestTypes;
+
+namespace Simplify.Web.Tests.Model.Binding.Binders;
+
+#if NET8_0_OR_GREATER
+
+[TestFixture]
+public class HttpQueryModelBinderTests
+{
+	[Test]
+	public void BindAsync_NotMatchedRequestMethod_NotBoundAndModelIsNull()
+	{
+		// Arrange
+
+		var context = Mock.Of<IWebContext>(x => x.Request.Method == "POST");
+		var args = new ModelBinderEventArgs<FooModel>(context);
+
+		// Act
+		new HttpQueryModelBinder().BindAsync(args);
+
+		// Assert
+
+		Assert.That(args.IsBound, Is.False);
+		Assert.That(args.Model, Is.Null);
+	}
+
+	[Test]
+	public void BindAsync_MatchedRequestMethodAndDataExist_Bound()
+	{
+		// Arrange
+
+		var query = new Dictionary<string, StringValues>
+		{
+			{ "ID", "1" },
+			{ "Name", "Bar" }
+		};
+
+		var context = Mock.Of<IWebContext>(x =>
+			x.Request.Method == "GET" &&
+			x.Query == new QueryCollection(query));
+
+		var args = new ModelBinderEventArgs<FooModel>(context);
+
+		// Act
+		new HttpQueryModelBinder().BindAsync(args);
+
+		// Assert
+
+		Assert.That(args.IsBound, Is.True);
+		Assert.That(args.Model, Is.Not.Null);
+		Assert.That(args.Model.ID, Is.EqualTo(1));
+		Assert.That(args.Model.Name, Is.EqualTo("Bar"));
+	}
+
+	[Test]
+	public void BindAsync_MatchedRequestMethodAndNoDataExist_BoundWithDefaultValues()
+	{
+		// Arrange
+
+		var query = new Dictionary<string, StringValues>
+		{
+		};
+
+		var context = Mock.Of<IWebContext>(x =>
+			x.Request.Method == "GET" &&
+			x.Query == new QueryCollection(query));
+
+		var args = new ModelBinderEventArgs<FooModel>(context);
+
+		// Act
+		new HttpQueryModelBinder().BindAsync(args);
+
+		// Assert
+
+		Assert.That(args.IsBound, Is.True);
+		Assert.That(args.Model, Is.Not.Null);
+		Assert.That(args.Model.ID, Is.EqualTo(0));
+		Assert.That(args.Model.Name, Is.EqualTo(null));
+	}
+}
+
+#endif
---
+++ src/Simplify.Web.Tests/Model/Binding/Binders/TestTypes/FooModel.cs
@@ -0,0 +1,8 @@
+namespace Simplify.Web.Tests.Model.Binding.Binders.TestTypes
+{
+	public class FooModel
+	{
+		public int ID { get; set; }
+		public string? Name { get; set; }
+	}
+}
